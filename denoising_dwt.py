569c21feb64c5b8fb2b32bcd1d11f09b5813a5f7
aditya-834 <arangarajan834@gmail.com>
2018-10-04 19:28:20 +0530

    -

--- denoising_dwt.py
+++ denoising_dwt.py
@@ -4,15 +4,17 @@ import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 fs, data = wavfile.read('H:/dataset/msjs1/msjs1/audio/sa1.wav')
-w = pywt.Wavelet('sym4')
+w = pywt.Wavelet('bior6.8')
 data_dwt_max_val = pywt.dwt_max_level(len(data),w)
 print(data_dwt_max_val)
-coeffs = pywt.wavedec(data,w,level = 2)
-Ca2,Cd2,Cd1 = coeffs
-Ca2t = pywt.threshold(Ca2,np.std(Ca2)/2,mode= 'soft')
-Cd2t = pywt.threshold(Cd2, np.std(Cd2),mode= 'less')
-Cd1t = pywt.threshold(Cd1, np.std(Cd1),mode= 'less')
-denoised_data = pywt.waverec([Ca2t,Cd2t,Cd1t], w, mode='symmetric', axis=-1)
+coeffs = pywt.wavedec(data,w,level = 4)
+Ca2,Cd4,Cd3,Cd2,Cd1 = coeffs
+#Ca2t = pywt.threshold(Ca2,np.std(Ca2)/2,mode= 'soft')
+Cd4t = pywt.threshold(Cd4, 20000,mode= 'hard')
+Cd3t = pywt.threshold(Cd3, 20000,mode= 'hard')
+Cd2t = pywt.threshold(Cd2, 20000,mode= 'hard')
+Cd1t = pywt.threshold(Cd1, 20000,mode= 'hard')
+denoised_data = pywt.waverec([Ca2,Cd4t,Cd3t,Cd2t,Cd1t], w, mode='symmetric', axis=-1)
 plt.plot(data)
 plt.plot(denoised_data)
 plt.show()

e340f75968d5e8717fa357fd6949aef186bb468c
aditya-834 <arangarajan834@gmail.com>
2018-10-07 22:51:57 +0530

    Update mmsestfa.py

--- mmsestfa.py
+++ mmsestfa.py
@@ -21,7 +21,7 @@ def vad(signal,noise,NoiseCounter):
     Hangover = 8
     FreqResol = len(signal)
     SpectralDist = 20*(np.log10(signal)-np.log10(noise))
-    SpectralDist[np.where(SpectralDist<0)] = 0
+    SpectralDist[np.argwhere(SpectralDist<0)] = 0
     Dist = np.mean(SpectralDist)
     if Dist<NoiseMargin:
         NoiseFlag = 1
@@ -33,8 +33,29 @@ def vad(signal,noise,NoiseCounter):
         SpeechFlag = 0
     else:
         SpeechFlag = 1
-    return NoiseFlag, SpeechFlag, NoiseCounter, Dist   
-
+    return NoiseFlag, SpeechFlag, NoiseCounter, Dist
+def OverlapAdd2(XNEW,Yphase,windowLen,ShiftLen):
+    if np.fix(ShiftLen)!=ShiftLen:
+        ShiftLen = np.fix(ShiftLen)
+    A = np.shape(XNEW)
+    FrameNum = A[0]
+    FreqRes = A[1]
+    print(A)
+    Spec = XNEW*np.exp(-1j*Yphase)
+    if windowLen%2 == 0:
+        Spec = np.column_stack((Spec ,np.fliplr(np.conj(Spec[:,1:np.shape(Spec)[1]-1]))))
+    else:
+        Spec = np.column_stack((Spec ,np.fliplr(np.conj(Spec[:,1:np.shape(Spec)[1]-2]))))
+    sig=np.zeros(int((FrameNum-1)*ShiftLen+windowLen))
+    print(np.shape(sig))
+    weight=sig
+    #print(type(Spec))
+    for i in range(0,FrameNum):
+        start=(i)*ShiftLen   
+        spec=Spec[int(i),:]
+        sig[int(start):int(start+windowLen)] = sig[int(start):int(start+windowLen)]+ np.real(np.fft.ifft(spec,windowLen))
+        #print(np.shape(sig2))
+    return sig        
         
 fs, data = wavfile.read('H:/dataset/msjs1/msjs1/audio/sa1.wav')
 IS = 0.25
@@ -76,11 +97,13 @@ for i in range(0, numberofFrames):
     xi=alpha*(np.square(G))*Gamma+(1-alpha)*np.max(gammaNew-1,0)
     Gamma = gammaNew
     nu = Gamma*xi/(1+xi)
-    G = (Gamma1p5*np.sqrt(nu)/Gamma)*np.exp(-nu/2)*((1+nu)*sp.jv(0,nu/2)+nu*sp.jv(1,nu/2)      
-    #np.where(np.isnan(G) or np.isinf(G))
-    G[np.where(np.isnan(G) or np.isinf(G))] = xi[np.where(np.isnan(G) or np.isinf(G))]/(1+xi[np.where(np.isnan(G) or np.isinf(G))])
+    G = (Gamma1p5*np.sqrt(nu)/Gamma)*np.exp(-nu/2)*((1+nu)*sp.jv(0,nu/2)+nu*sp.jv(1,nu/2))      
+    Indx = np.argwhere(np.logical_or(np.isnan(G),np.isinf(G)))
+    G[Indx] = xi[Indx]/(1+xi[Indx])
     X[i,:] = G*Y[i,:]
-#output = OverlapAdd2(X,Yphase,W,SP*W)
-#signal = sig.lfilter(np.array([1.0],np.array([1.0 -pre_emph])),output)                                                    
-#plt.plot(y[0])
-#plt.show()
+output = OverlapAdd2(X,Yphase,W,SP*W)
+signal = sig.lfilter(np.array([1.0]),np.array([1.0 -pre_emph]),output)
+
+plt.plot(data)
+plt.plot(signal)                                                    
+plt.show()
